fac1af4d7ef47e28d8fbbcbc95ab7f7b387fcd26
diipacheco <edilsonpacheco6@gmail.com>
2025-10-21 18:39:19 -0300

    Adds eslint/prettier and fastify structure

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import prettier from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: {
+      js,
+      prettier: prettierPlugin,
+    },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+  ...tseslint.configs.recommended,
+  prettier,
+]);
